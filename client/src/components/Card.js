85eb75279e7e4fe8b7950542d0ab3064e38d60a5
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-31 19:47:23 -0400

    Updated Carousel to show images

--- client/src/components/Card.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Card = ({ number }) => <div className="card">{number}</div>
-
-export default  Card;
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import '../styles/heroStyles.css';
-import Card from './Card'
 import Carousel from "react-elastic-carousel"
+//Just import advertisement images here
+import add1 from '../Images/add1.jpg';
+import add2 from '../Images/add2.jpg';
+import add3 from '../Images/add3.jpg';
+import add4 from '../Images/add4.jpg';
 
 export default function showCarousel(){
-    const breakPoints = [
-        {width: 1, itemsToShow: 1},
-        {width: 500, itemsToShow: 2},
-        {width: 768, itemsToShow: 3},
-        {width: 1200, itemsToShow: 4},
-    ];
 
     return(
-        <div className="carousel">
-            <Carousel breakPoints={breakPoints}>
-                <Card number="1"/>
-                <Card number="2"/>
-                <Card number="3"/>
-                <Card number="4"/>
-                <Card number="5"/>
-                <Card number="6"/>
-                <Card number="7"/>
-                <Card number="8"/>
+        <div className="carousel" >
+            <Carousel>
+                <div>
+                    <img src={add1}/>
+                </div>
+                <div>
+                    <img src={add2}/>
+                </div>
+                <div>
+                    <img src={add3}/>
+                </div>
+                <div>
+                    <img src={add4}/>
+                </div>
             </Carousel>
         </div>
     );
