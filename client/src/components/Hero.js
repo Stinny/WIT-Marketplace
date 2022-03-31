6e6093ba8d21176ecf502dbe5bd5cd695e1b8e3f
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-31 18:43:25 -0400

    Added carousel

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import '../styles/heroStyles.css';
+import Card from './Card'
+import Carousel from "react-elastic-carousel"
 
-const Hero = () => {
-  return (
-    <div className='hero-con'>
-      <div className='hero-wrap'>
-        <h1 style={{ textAlign: 'center' }}>Hero Section</h1>
-        <p style={{ textAlign: 'center' }}>
-          can make this whatever we want, ads, info about the marketplace, etc.
-        </p>
-      </div>
-    </div>
-  );
-};
+export default function showCarousel(){
+    const breakPoints = [
+        {width: 1, itemsToShow: 1},
+        {width: 500, itemsToShow: 2},
+        {width: 768, itemsToShow: 3},
+        {width: 1200, itemsToShow: 4},
+    ];
 
-export default Hero;
+    return(
+        <div className="carousel">
+            <Carousel breakPoints={breakPoints}>
+                <Card number="1"/>
+                <Card number="2"/>
+                <Card number="3"/>
+                <Card number="4"/>
+                <Card number="5"/>
+                <Card number="6"/>
+                <Card number="7"/>
+                <Card number="8"/>
+            </Carousel>
+        </div>
+    );
+}
