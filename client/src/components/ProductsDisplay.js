de3a88996137c1f2e603d7e7b108a3f45ddf1e96
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-04-01 01:13:37 -0400

    Added Carousel of recommended items

--- client/src/components/ProductsDisplay.js
+++ client/src/components/ProductsDisplay.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import '../styles/productsDisplayStyles.css';
 import ItemView from './ItemView'
+import Carousel from "react-elastic-carousel";
 
 const ProductsDisplay = () => {
+    const breakPoints = [
+        {width: 1, itemsToShow: 1},
+        {width: 500, itemsToShow: 2},
+        {width: 1200, itemsToShow: 3},
+        {width: 1500, itemsToShow: 4},
+    ]
+
   return (
     <div>
       <div className="recommendedArea">
           <p>Recommended for you</p>
           <div className="item">
-              <ItemView/><ItemView/><ItemView/><ItemView/>
+              <Carousel className="parent" breakPoints={breakPoints}>
+                  <ItemView className="child"/>
+                  <ItemView className="child"/>
+                  <ItemView className="child"/>
+                  <ItemView className="child"/>
+                  <ItemView className="child"/>
+                  <ItemView className="child"/>
+              </Carousel>
           </div>
       </div>
     </div>
