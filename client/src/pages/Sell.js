5c9d434b3301d8ddbc684ca9f8c6152c4e77a046
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-31 21:47:01 -0400

    Added seller page

--- client/src/pages/Sell.js
+++ client/src/pages/Sell.js
@@ -1,21 +1,89 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import '../styles/sellStyles.css';
 
-const Login = () => {
-    return (
-        <div className='login-con'>
-            <div className='login-wrap'>
-                <h2>What do you want to sell?</h2>
-                <div className='inputs-con'>
-                    <input type='text' placeholder='What' className='login-in' />
-                    <input type='text' placeholder='Price?' className='login-in' />
-                </div>
-                <button type='submit' className='login-btn'>
-                    SELL
-                </button>
-            </div>
-        </div>
-    );
-};
+const formReducer = (state, event) => {
+    if(event.reset) {
+        return {
+            apple: '',
+            count: 0,
+            name: '',
+            'gift-wrap': false,
+        }
+    }
+    return {
+        ...state,
+        [event.name]: event.value
+    }
+}
 
-export default Login;
+function SellItem() {
+    const [formData, setFormData] = useReducer(formReducer, {
+        //Set default data
+        stock: 1
+    });
+    const [submitting, setSubmitting] = useState(false);
+
+    const validate = e => {
+        //e.preventDefault();
+        //Validate input
+    }
+
+    const updateChange = e => {
+        setFormData({
+            name: e.target.name,
+            value: e.target.value,
+        });
+    }
+
+    return(
+        <div className="wrapper">
+            <h1 id='pageTitle'>Just a couple questions</h1>
+            <form onSubmit={validate}>
+                <fieldset>
+                    <label>
+                        <p>Add Title</p>
+                        <input id="productTitle" name="name" onChange={updateChange} value={formData.name || ''}/>
+                    </label>
+                    <label>
+                        <p>Price</p>
+                        <input name="price" onChange={updateChange} value={formData.price || ''}/>
+                    </label>
+                </fieldset>
+                <fieldset>
+                    <label>
+                        <p>Pictures</p>
+                        <input name="pictures" onChange={updateChange} placeholder="How do we add them?" value={formData.pictures || ''}/>
+                    </label>
+                </fieldset>
+                <fieldset>
+                    <label>
+                        <p>Condition</p>
+                        <select name="condition" onChange={updateChange} value={formData.condition || ''}>
+                            <option value="">--New or used?--</option>
+                            <option value="new">New</option>
+                            <option value="used">Used</option>
+                        </select>
+                    </label>
+                    <label>
+                        <p>Available Stock</p>
+                        <input id="stock" type="number" name="stock" onChange={updateChange} step="1" value={formData.stock || ''}/>
+                    </label>
+                </fieldset>
+
+                <fieldset>
+                    <label>
+                        <p>Description</p>
+                        <textarea id="des" name="des" onChange={updateChange} value={formData.des || ''}
+                        placeholder="Describe your item thoroughly, buyers love information."/>
+                    </label>
+                </fieldset>
+
+
+
+                <button id="sell-button" type="submit" disabled={submitting}>DONE</button>
+            </form>
+        </div>
+    )
+}
+
+export default SellItem;
