5083bb6b1fb548bbcdcb1dcfa958a79d38b66756
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-04-01 22:47:20 -0400

    Adding top bar

--- client/src/pages/Listing.js
+++ client/src/pages/Listing.js
@@ -4,34 +4,63 @@ import ItemView from '../components/ItemView'
 
 const Cart = () => {
     return (
-        <div class='page-con'>
-            <div class='listings-con'>
-                <div class='item'>
-                    <ItemView/>
+        <div id='page-container'>
+            <div className='top-bar'>
+                    <div id='filters-title'>
+                        <h3>
+                        Filters
+                        </h3>
+                    </div>
+                    <div id='list-area'>
+                        <h3 id='list-title'>Results</h3>
+                            <p><strong>Sort by</strong></p>
+                            <select id="sort-by">
+                                <option value="mr">Most Relevant</option>
+                                <option value="lp">Lowest Price</option>
+                                <option value="hp">Highest Price</option>
+                            </select>
+                    </div>
+            </div>
+
+            <div class='page-con'>
+                <div class='filter-con'>
+                    <div class='item'>
+                        <p>Text</p>
+                    </div>
                 </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
-                </div>
-                <div className='item'>
-                    <ItemView/>
+
+                <div class='listings-con'>
+                    <div class='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+                    <div className='item'>
+                        <ItemView/>
+                    </div>
+
                 </div>
             </div>
         </div>
