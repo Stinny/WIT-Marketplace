9a5096641525a12e968abbff2ca2b4ee6b71838d
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-04-02 01:42:11 -0400

    Working on page change

--- client/src/pages/Listing.js
+++ client/src/pages/Listing.js
@@ -2,9 +2,10 @@ import React from 'react';
 import '../styles/listingStyles.css';
 import ItemView from '../components/ItemView'
 
-const Cart = () => {
+const Listings = () => {
     return (
         <div id='page-container'>
+
             <div className='top-bar'>
 
                 <div id='filters-title'>
@@ -28,10 +29,12 @@ const Cart = () => {
                     </div>
                 </div>
             </div>
+
             <div className='page-con'>
 
                 <div className='filter-con'>
-                    <div className="filters-text">
+
+                    <div id="price-filter">
                         <p className="filters-title">Price filter</p>
                         <button className='filter'>Maximum $100 </button> <br/>
                         <button className='filter'>Minimum $10 </button> <br/>
@@ -41,6 +44,47 @@ const Cart = () => {
                             <input className='price-range' type='text' placeholder='max'/>
                             <button className='go'>></button>
                         </div>
+
+                    </div>
+
+                    <div id="condition-filter">
+                        <p className="filters-title">Condition</p>
+                        <button className='filter'>New </button> <br/>
+                        <button className='filter'>Used </button> <br/>
+                    </div>
+
+                    <div id="brand-filter">
+                        <p className="filters-title">Brand</p>
+                        <button className='filter'>Apple </button> <br/>
+                        <button className='filter'>Samsung </button> <br/>
+                        <button className='filter'>Lenovo </button> <br/>
+                        <button className='filter'>LG </button> <br/>
+                        <button className='filter blue'>See all</button> <br/>
+                    </div>
+
+                    <div id="item-specific-filter">
+                        <p className="filters-title">Item Specific Filters</p>
+                        <p className="filters-title">Filter A</p>
+                        <button className='filter'>Option 1 </button> <br/>
+                        <button className='filter'>Option 2 </button> <br/>
+                        <button className='filter'>Option 3 </button> <br/>
+                        <button className='filter'>Option 4 </button> <br/>
+                        <button className='filter blue'>See all</button> <br/>
+
+                        <p className="filters-title">Filter B</p>
+                        <button className='filter'>Option 1 </button> <br/>
+                        <button className='filter'>Option 2 </button> <br/>
+                        <button className='filter'>Option 3 </button> <br/>
+                        <button className='filter'>Option 4 </button> <br/>
+                        <button className='filter blue'>See all</button> <br/>
+
+                        <p className="filters-title">Filter C</p>
+                        <button className='filter'>Option 1 </button> <br/>
+                        <button className='filter'>Option 2 </button> <br/>
+                        <button className='filter'>Option 3 </button> <br/>
+                        <button className='filter'>Option 4 </button> <br/>
+                        <button className='filter blue'>See all</button> <br/>
+
                     </div>
                 </div>
 
@@ -131,9 +175,17 @@ const Cart = () => {
                     </div>
 
                 </div>
+
+            </div>
+
+            <div id='page-select'>
+                <div>
+                    <button className='filter blue'>previous</button> <br/>
+                    <button className='filter blue'>next</button> <br/>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Cart;
+export default Listings;
